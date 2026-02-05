fe4faf62c31ecad10022de72d2e2d2317fdade5c
Krishna Mittal <krishnarajni234@gmail.com>
2026-02-05 21:25:05 +0530

    Update prog1.java
    Learnt About the scanner method that helps in taking input from the user

--- src/prog1.java
+++ src/prog1.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class prog1 {
     public static void main(String[] args) {
         System.out.println("Welcome to my first program");
+        Scanner s= new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = s.nextInt();
+        System.out.print("Enter second number: ");
+        int b = s.nextInt();
+        System.out.println("The resultant addition is : "+(a+b));
     }
 }
