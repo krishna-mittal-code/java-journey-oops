12c3d045f606355e7b4cb8becdc84bea52a3285a
Krishna Mittal <krishnarajni234@gmail.com>
2026-02-05 22:36:31 +0530

    more on array

---
+++ src/array2.java
@@ -0,0 +1,11 @@
+public class array2 {
+    public static void main(String[] args) {
+        int [] marks = {43,76,97,98};
+        System.out.println(marks.length);  //for length of array--- array_name.length----->no parenthesis after/before
+
+        // displaying array using for-each loop
+        for(int j: marks){
+            System.out.println(j);
+        }
+    }
+}
