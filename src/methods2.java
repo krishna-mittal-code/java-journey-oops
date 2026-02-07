f0385706a5be78e9e93b9156d72808b154059920
Krishna Mittal <krishnarajni234@gmail.com>
2026-02-07 21:51:09 +0530

    Method Overloading and Multiple Arguments

--- src/methods2.java
+++ src/methods2.java
@@ -10,10 +10,10 @@ public class methods2 {
             System.out.println("Hello "+i);  // a void function with same name but cahnge in  parameters.
         }
     }
-    // keepin mind you cannot change the returm type in method overloading
+    // keep in mind you cannot change the returm type in method overloading
 
 
-    // MULTIPKLE ARGUMENTS IN FUNCTION ............TAKES AN ARRAY OF ARGUMENTS AND PROCEEDS FURTHER
+    // MULTIPLE ARGUMENTS IN FUNCTION ............TAKES AN ARRAY OF ARGUMENTS AND PROCEEDS FURTHER
     static int add(int ...arr){
         int result=0;
         for(int i:arr){
@@ -26,7 +26,7 @@ public class methods2 {
         greet();
         greet(3);
 
-        int k=add(1,2,3,4,5,6,7,8,9);
+        int k=add(1,2,3,4,5,6,7,8,9);  //giving multiple arguments in the function
         System.out.println(k);
     }
     
